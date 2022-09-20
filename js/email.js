c4fc8d4e4482afad7a4d3d1bc93b1fad17ac0347
KAZI SAYED HOSSAIN <sayedmasud69@gmail.com>
2022-09-21 00:42:37 +0600

    Add files via upload

---
+++ js/email.js
@@ -0,0 +1,23 @@
+function send_email()
+{
+	var email_address = "sayedmasud69@gmail.com";
+	var email_subject = document.getElementById("subject").value;
+	var email_body = document.getElementById("message").value;
+
+	if(email_subject=="")
+	{
+		email_subject = "Contact from Portfolio Website";
+	}
+
+	var url = "https://mail.google.com/mail/?view=cm&fs=1&to="+email_address+"&su="+email_subject+"&body="+email_body;
+	window.location.href = url;
+
+	//------------------------------//
+	
+	/*$("#btn_send_message").click(function() {
+        $("<a>").prop({
+            target: "_blank",
+            href: url
+        })[0].click();
+    });*/
+}
